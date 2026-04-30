6021a1a976402ec86d10c894b5273c0d9cfdbf09
alex-rojas26 <boza3511@stthomas.edu>
2026-04-30 00:10:41 +0000

    Relationships between restaurant and foo

--- models/food.js
+++ models/food.js
@@ -1,9 +1,22 @@
 import mongoose from "mongoose";
 const foodSchema = new mongoose.Schema({
-    restName: { type: String, required: true },
-    meal: { type: String, required: true },
-    rating: { type: Number, min: 1, max: 10 },
-    comments: { type: String, required: false },
-    visitDate: { type: Date, default: Date.now }
+    restName: { 
+        type: mongoose.Schema.Type,
+        ref: "Restaurant", 
+        required: [true, "Restaurant name is required"] },
+    meal: { 
+        type: String, 
+        required: [true, "Name of meal is required"] },
+    rating: { 
+        type: Number, 
+        min: [1, "A rating from 1 to 10"], 
+        max: [10, "A rating from 1 to 10"],
+        required: false },
+    comments: { 
+        type: String, 
+        required: false },
+    visitDate: { 
+        type: Date, 
+        default: Date.now }
 });
 export default mongoose.model("Food", foodSchema);
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
 const restSchema = new mongoose.Schema({
-    restName: { type: String, required: true },
-    cuisine: { type: String, required: true },
-    rating: { type: Number, min: 1, max: 10 },
-    visitDate: { type: Date, default: Date.now }
+    restName: { 
+        type: String, 
+        required: [true, "Restaurant name is required"] },
+    cuisine: { 
+        type: String, 
+        required: false },
+    rating: { 
+        type: Number, 
+        min: [1, "A rating from 1 to 10"], 
+        max: [10, "A rating from 1 to 10"],
+        required: false },
+    visitDate: { 
+        type: Date, 
+        default: Date.now }
 });
-export default mongoose.model("Restaurants", restSchema);
+export default mongoose.model("Restaurant", restSchema);
